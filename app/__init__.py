174c3248e2d0684f94e9bd2d5dca27f7e65bd84f
mkiterian <hmukite@gmail.com>
2017-08-17 13:57:38 +0300

    set config in env

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config.from_object('config.DevelopmentConfig')
+app.config.from_object(os.environ['CURRENT_CONFIG'])
+
 db = SQLAlchemy(app)
 
 from . import models
